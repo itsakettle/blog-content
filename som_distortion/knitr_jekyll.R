e75f75ef7b4e2d3415aafa025a84bcfdad28385d
itsakettle <you@example.com>
2017-07-03 22:51:20 +0100

    som_distortion post update

--- som_distortion/knitr_jekyll.R
+++ som_distortion/knitr_jekyll.R
@@ -1,5 +1,4 @@
-knitr::knit(input='~/Google Drive/som_distortion/som_distortion.Rmd', 
-            output='~/Moon/_posts/2017-06-23-som_distortion.md')
-
+knitr::knit(input='~/blog-content/som_distortion/som_distortion.Rmd', 
+            output='~/itsakettle.github.io/_posts/2017-06-23-som_distortion.md')
 
 
