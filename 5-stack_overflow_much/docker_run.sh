41c04f24817968e3c61c60b62e019cea28f0851a
Will Rogers <itsakettle@gmail.com>
2024-02-16 16:51:00 +0000

    modify script

--- 5-stack_overflow_much/docker_run.sh
+++ 5-stack_overflow_much/docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 image_name="stack_overflow_much_$1"
-docker build -t $image_name -f "./$1" . && docker run -it --rm $image_name
+docker build -t $image_name -f "./$1" . && docker run -t --rm $image_name
 echo $?
---
+++ 6-docker_build_run_remove/main.py
@@ -0,0 +1,7 @@
+import polars as pl
+
+def main():
+    print("HELLO!!!")
+
+if __name__ == "__main__":
+    main()
