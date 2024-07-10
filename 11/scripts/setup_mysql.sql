e4c7bec35f2e8dd60925bbe5344d68f642c225cc
Will Rogers <itsakettle@gmail.com>
2024-07-10 06:37:49 +0100

    some progress

--- 11/scripts/setup_mysql.sql
+++ 11/scripts/setup_mysql.sql
@@ -1,3 +1,8 @@
 CREATE TABLE treasure_locations (
-  
-)
+  location NVARCHAR(100) NOT NULL,
+  feather_code NVARCHAR(100) NOT NULL
+);
+
+INSERT INTO treasure_locations VALUES 
+  ("what3words 1", "sparrow_123"),
+  ("what3words 2", "owl_123");
--- 11/treasure_api.py
+++ 11/treasure_api.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Depends, Header, HTTPException
 import pymysql
 
 app = FastAPI()
+
+
 connection_parameters = \
     {
     'host': 'localhost',
@@ -11,7 +13,11 @@ connection_parameters = \
     'cursorclass': pymysql.cursors.DictCursor
     }
 
+
+# No hashing+salting of feather codes because this is a simple sql injection example app.
+# We assume Crow has created the database entries somehow as there is no endpoint for this
 insecure_sql = "SELECT location from locations where feather_code = '{feather_code}'"
+
 
 def get_locations_from_db(feather_code: str):
     
@@ -20,7 +26,6 @@ def get_locations_from_db(feather_code: str):
             sql = insecure_sql.format_map({'feather_code': feather_code})
             locations_list_of_dict = cursor.execute(sql).fetch_all()
             return [location_dict['location'] for location_dict in locations_list_of_dict]
-    
 
 @app.get("/my-treasure-locations")
 async def locations(x_feather_code: Header(...)):
