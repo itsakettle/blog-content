88b58c10517cf042532ded906934a12901cbc1b3
Will Rogers <itsakettle@gmail.com>
2024-07-12 00:49:19 +0100

    treasure api

--- 11/scripts/setup_mysql.sh
+++
@@ -1,2 +0,0 @@
-mysql -u username -p -h localhost db < setup_mysql.sql
-touch init_complete
--- 11/scripts/setup_mysql.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE treasure_locations (
-  location NVARCHAR(100) NOT NULL,
-  feather_code NVARCHAR(100) NOT NULL
-);
-
-INSERT INTO treasure_locations VALUES 
-  ("what3words 1", "sparrow_123"),
-  ("what3words 2", "owl_123");
