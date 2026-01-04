80082a76095a8bdaed27ac9e3b611821c58802f2
Will Rogers <itsakettle@gmail.com>
2026-01-04 13:03:39 +0000

    18 progress

--- 18/README.md
+++ 18/README.md
@@ -8,7 +8,12 @@ docker compose up -d
 
 ### Run population script
 ```bash
-docker compose exec app python populate.py
+docker compose exec app python populate_tables.py
+```
+
+###
+```bash
+docker exec -it tea-mysql mysql -u root -pNotSensitive
 ```
 
 ### Stop containers
@@ -19,6 +24,11 @@ docker compose down
 ### Stop and remove everything including data
 ```bash
 docker compose down -v
+```
+
+### Rebuild
+```bash
+docker-compose build --no-cache
 ```
 
 
--- 18/python/populate_tables.py
+++ 18/python/populate_tables.py
@@ -6,9 +6,10 @@ from datetime import datetime, timedelta
 import mysql.connector
 
 PEOPLE_COUNT = 1000
-DAYS_BACK = 30
+DAYS_BACK = 365
 MADE_CUPS_COUNT = 500000
 DRANK_CUPS_COUNT = 800000
+INSERT_BATCH_SIZE = 100000
 
 # ---- DB CONFIG ----
 conn = mysql.connector.connect(
@@ -17,7 +18,6 @@ conn = mysql.connector.connect(
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_NAME")
 )
-cursor = conn.cursor()
 
 # ---- DATA ----
 tea_types = ["black", "green", "herbal", "oolong", "chai", "earl_grey"]
@@ -27,10 +27,26 @@ now = datetime.now()
 def random_time(days_back=DAYS_BACK):
     return now - timedelta(
         days=random.randint(0, days_back),
-        minutes=random.randint(0, 1440)
+        minutes=random.randint(0, 1440),
+        seconds=random.randint(0, 60),
+        milliseconds=random.randint(0, 1000)
     )
 
+def batch_insert(conn, table, columns, data, batch_size=INSERT_BATCH_SIZE):
+    cursor = conn.cursor()
+    col_str = ', '.join(columns)
+    placeholder = ', '.join(['%s'] * len(columns))
+    sql = f"INSERT INTO {table} ({col_str}) VALUES ({placeholder})"
+
+    for i in range(0, len(data), batch_size):
+        batch = data[i:i + batch_size]
+        cursor.executemany(sql, batch)
+        conn.commit()
+
+    cursor.close()
+
 # ---- INSERT MADE CUPS ----
+print("Creating made cups")
 made_rows = []
 for _ in range(MADE_CUPS_COUNT):
     made_rows.append((
@@ -39,16 +55,16 @@ for _ in range(MADE_CUPS_COUNT):
         random_time()
     ))
 
-cursor.executemany(
-    """
-    INSERT INTO made_cups_of_tea (tea_type, person_id, made_at)
-    VALUES (%s, %s, %s)
-    """,
-    made_rows
-)
+print("Inserting made cups")
+batch_insert(conn,
+             table='made_cups_of_tea',
+             columns=['tea_type', 'person_id', 'made_at'],
+             data=made_rows)
+
 
 # ---- INSERT DRANK CUPS ----
 drank_rows = []
+print("Creating drank cups")
 for _ in range(DRANK_CUPS_COUNT):
     drank_rows.append((
         random.choice(tea_types),
@@ -56,16 +72,12 @@ for _ in range(DRANK_CUPS_COUNT):
         random_time()
     ))
 
-cursor.executemany(
-    """
-    INSERT INTO drank_cups_of_tea (tea_type, person_id, drank_at)
-    VALUES (%s, %s, %s)
-    """,
-    drank_rows
-)
+print("Inserting drank cups")
+batch_insert(conn,
+             table='drank_cups_of_tea',
+             columns=['tea_type', 'person_id', 'drank_at'],
+             data=drank_rows)
 
-conn.commit()
-cursor.close()
 conn.close()
 
 print("Tea data successfully poured ☕")
